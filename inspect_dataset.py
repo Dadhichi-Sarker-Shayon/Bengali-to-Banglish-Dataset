4b1fd81ca0ac9494cdd5be7c6a464422bb6ee5ae
Dadhichi-Sarker-Shayon <www.dadhichipk123@gmail.com>
2026-07-24 23:54:20 +0600

    Remove unnecessary files, keep only README and CSVs

--- inspect_dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("D:/BN-BE-EN/bengali_dataset.csv")
-
-padi = df[df['Banglish'].str.contains('padi', na=False)].head(15)
-
-with open("D:/BN-BE-EN/output3.txt", "w", encoding="utf-8") as f:
-    f.write("Rows with padi:\n")
-    for i, row in padi.iterrows():
-        f.write(f"{row['Bangla_Word']} -> {row['Banglish'][:70]}\n")
